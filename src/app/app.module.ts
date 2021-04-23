6ed58cf15d931172cdad5af56dab30e0f53d444e
R-DURSO <rdurso@outlook.fr>
2021-04-23 15:06:47 +0200

    premeir changement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
 import { SaerchBarComponent } from './view/saerch-bar/saerch-bar.component';
 import { ResultComponent } from './view/result/result.component';
 import { MainpageComponent } from './view/mainpage/mainpage.component';
 import { CurveComponent } from './service/curve/curve.component';
 import { SelectComponent } from './select/select.component';
+import  {RouterModule, Routes} from "@angular/router";
+
+
+const appRoutes : Routes =[
+    {path : 'result', component : ResultComponent },
+    {path : 'main', component : MainpageComponent  },
+    {path : '', component : MainpageComponent}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent,
     SaerchBarComponent,
     ResultComponent,
     MainpageComponent,
@@ -22,7 +29,8 @@ import { SelectComponent } from './select/select.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
